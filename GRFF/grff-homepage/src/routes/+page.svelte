fd84b6a93e8524b38c0a00f32c308ee38d18bc3a
Abhay Rai <abhay.rai@amway.com>
2026-04-06 19:39:05 +0530

    created the homepage using Svelte

---
+++ GRFF/grff-homepage/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Navbar from '$lib/components/Navbar.svelte';
+  import HeroSection from '$lib/components/HeroSection.svelte';
+  import StatsSection from '$lib/components/StatsSection.svelte';
+  import FeaturedCoursesSection from '$lib/components/FeaturedCoursesSection.svelte';
+  import FeaturesSection from '$lib/components/FeaturesSection.svelte';
+  import CTASection from '$lib/components/CTASection.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<main class="min-h-screen bg-[#05010d] text-white">
+  <Navbar />
+  <HeroSection />
+  <StatsSection />
+  <FeaturedCoursesSection />
+  <FeaturesSection />
+  <CTASection />
+  <Footer />
+</main>
